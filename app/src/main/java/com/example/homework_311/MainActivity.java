9457a3ff140288909c13dcca208dde97116a0cb9
GAVR <axotnick@bk.ru>
2020-05-29 11:01:30 +0300

    settings mod

--- app/src/main/java/com/example/homework_311/MainActivity.java
+++ app/src/main/java/com/example/homework_311/MainActivity.java
@@ -1,29 +1,41 @@
 package com.example.homework_311;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
+import android.widget.ImageView;
+import java.io.File;
+import java.util.Objects;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+    private static final int SETTINGS_REQUEST_CODE = 1;
+    private ImageView background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        background = findViewById(R.id.imageView);
         Button btnSet = findViewById(R.id.btnSet);
         btnSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,
                         SettingsActivity.class);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, SETTINGS_REQUEST_CODE);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SETTINGS_REQUEST_CODE && data != null) {
+            File file = (File) data.getSerializableExtra(SettingsActivity.IMAGE_RESULT_KEY);
+            Objects.requireNonNull(file);
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            background.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/com/example/homework_311/SettingsActivity.java
+++ app/src/main/java/com/example/homework_311/SettingsActivity.java
@@ -1,64 +1,40 @@
 package com.example.homework_311;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
-import android.widget.ImageView;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
-
+import java.io.File;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import java.io.File;
-
 public class SettingsActivity extends AppCompatActivity {
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case 0:
-                if (resultCode == 1) {
-                    int permissionStatus = ContextCompat.checkSelfPermission(
-                            this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-                    if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
-                        loadImg();
-                        finish();
-                    } else {
-                        ActivityCompat.requestPermissions(this,
-                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                REQUEST_CODE_PERMISSION_WRITE_STORAGE
-                        );
-                    }
-                }
-                break;
-            default:
-                super.onActivityResult(requestCode, resultCode, data);
-                break;
-        }
-    }
-
-
-    public static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 100;
-
+    static final String IMAGE_RESULT_KEY = "IMAGE_RESULT_KEY";
+    private static final int REQUEST_CODE_PERMISSION_WRITE_STORAGE = 100;
+    private EditText fileInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-
+        fileInput = findViewById(R.id.editText);
+        findViewById(R.id.setBgBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissions();
+            }
+        });
+    }
+    private void checkPermissions() {
         int permissionStatus = ContextCompat.checkSelfPermission(
                 this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
         if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
             loadImg();
-            finish();
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
@@ -66,36 +42,39 @@ public class SettingsActivity extends AppCompatActivity {
             );
         }
     }
-
     private void loadImg() {
         if (isExternalStorageWritable()) {
+            String fileName = fileInput.getText().toString();
+            if (TextUtils.isEmpty(fileName)) {
+                Toast.makeText(this, "Введите имя файла.", Toast.LENGTH_SHORT).show();
+                return;
+            }
             File file = new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS), "image.jpg");
-            ImageView imageView = findViewById(R.id.imageView);
-            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-            imageView.setImageBitmap(bitmap);
+                    Environment.DIRECTORY_DOWNLOADS), fileName);
+            if (!file.exists()) {
+                Toast.makeText(this, "Файла с указанным именем не существует.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent result = new Intent();
+            result.putExtra(IMAGE_RESULT_KEY, file);
+            setResult(RESULT_OK, result);
+            finish();
         }
     }
-
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_PERMISSION_WRITE_STORAGE:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    loadImg();
-                } else {
-                    Toast.makeText(SettingsActivity.this, "error", Toast.LENGTH_SHORT);
-                }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSION_WRITE_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadImg();
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(SettingsActivity.this, "Нужно разрешение на чтение фалов", Toast.LENGTH_SHORT).show();
+            } else  {
+                Toast.makeText(SettingsActivity.this, "Включите разрешение на чтение файлов в настройках", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
